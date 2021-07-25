2820e3cbc4dc30ea070184f7c9b90537eca10be7
Joelhs <jhericoo8322@gmail.com>
2021-07-25 07:58:50 -0400

    Conectando a base de datos

--- app/src/main/java/com/joel/proyectogrado/SelectOptionAuthActivity.java
+++ app/src/main/java/com/joel/proyectogrado/SelectOptionAuthActivity.java
@@ -2,13 +2,37 @@ package com.joel.proyectogrado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
-
+    Toolbar mToolbar;
+    Button mButtonGoToLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+
+        mToolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Seleccionar opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mButtonGoToLogin=findViewById(R.id.btnGoToLogin);
+        mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+
+    }
+    public void goToLogin(){
+        Intent intent=new Intent(SelectOptionAuthActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
